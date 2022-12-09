e65b80e22503779b3900aecf86e3ca68a64e73a5
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-12-09 17:43:35 +0900

    グラフ表示のコンポーネントに選択された都道府県を親コンポーネントから渡して画面表示

--- src/components/charts/ChartComponent.vue
+++ src/components/charts/ChartComponent.vue
@@ -1,4 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import '@/assets/css/charts/chart.css'
+import { Prefecture } from '@/assets/ts/interfaces/interfaces'
+import { ref, watchEffect } from 'vue'
+
+interface Props {
+    selectedPrefectures: Prefecture[]
+}
+
+const props = defineProps<Props>()
+
+const prefectures = ref<Prefecture[]>([])
+watchEffect(() => (prefectures.value = props.selectedPrefectures))
+</script>
 <template lang="">
-    <div></div>
+    <section class="chart-area">
+        {{ prefectures }}
+    </section>
 </template>
--- src/views/PrefCharts.vue
+++ src/views/PrefCharts.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import '@/assets/css/prefCharts.css'
 import PrefectureComponent from '@/components/prefectures/PrefectureComponent.vue'
+import ChartComponent from '@/components/charts/ChartComponent.vue'
 import { Prefecture } from '@/assets/ts/interfaces/interfaces'
 import { ref } from 'vue'
 
@@ -10,12 +11,12 @@ const selectedPrefectures = ref<Prefecture[]>([])
 // 子コンポーネントの都道府県一覧のチェックボックスの選択内容を反映
 const onSelectPrefecture = (prefs: Prefecture[]): void => {
     selectedPrefectures.value = prefs
-    console.log(selectedPrefectures.value)
 }
 </script>
 
 <template>
     <main class="main-content">
         <PrefectureComponent @onSelectPrefecture="onSelectPrefecture" />
+        <ChartComponent :selectedPrefectures="selectedPrefectures" />
     </main>
 </template>
