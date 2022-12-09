2003d701ed424fc2de9e0c8dec1dfcc2549e2176
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-12-10 07:47:06 +0900

    強制更新するようにしたのでcomputed管理してたoptionを消去し簡略化

--- src/components/organisms/ChartComponent.vue
+++ src/components/organisms/ChartComponent.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import '@/assets/css/charts/chart.css'
 import VueHighcharts from 'vue3-highcharts'
-// import chartOptions from '@/assets/ts/chartOptions'
-import TestComponent from './TestComponent.vue'
+import chartOptions from '@/assets/ts/chartOptions'
 import { PrefInfo, PrefCharts } from '@/assets/ts/interfaces/interfaces'
-import { ref, toRefs, watchEffect, computed, nextTick } from 'vue'
+import { ref, toRefs, watchEffect, nextTick } from 'vue'
 
 interface Props {
     prefPopulation: PrefInfo[]
@@ -13,54 +12,6 @@ interface Props {
 const props = defineProps<Props>()
 // 都道府県人口情報
 const { prefPopulation } = toRefs(props)
-const prefCharts = ref<PrefCharts[]>([])
-
-const chartOptions = computed(() => ({
-    title: {
-        text: '都道府県の人口動態',
-    },
-    xAxis: {
-        categories: [...Array(18)].map((_, index) => 1960 + index * 5),
-    },
-    yAxis: {
-        title: {
-            text: '人口(人)',
-        },
-        plotLines: [
-            {
-                value: 0,
-                width: 1,
-                color: '#888',
-            },
-        ],
-    },
-    tooltip: {
-        valueSuffix: '人',
-    },
-    legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle',
-        borderWidth: 0,
-    },
-    series: prefCharts.value,
-    responsive: {
-        rules: [
-            {
-                condition: {
-                    maxWidth: 600,
-                },
-                chartOptions: {
-                    legend: {
-                        layout: 'horizontal',
-                        align: 'center',
-                        verticalAlign: 'bottom',
-                    },
-                },
-            },
-        ],
-    },
-}))
 
 // グラフ描画用の人口情報を生成
 const generatePrefCharts = (prefs: PrefInfo[]): PrefCharts[] => {
@@ -89,20 +40,19 @@ const forceRenderer = async () => {
 
 // 都道府県人口情報が更新された際チャートを更新
 watchEffect(() => {
-    prefCharts.value.splice(0)
-    generatePrefCharts(prefPopulation.value).map((pref) => {
-        prefCharts.value.push(pref)
-    })
-
+    chartOptions.series = generatePrefCharts(prefPopulation.value)
     forceRenderer()
 })
 </script>
 <template>
     <section class="chart-area">
         <VueHighcharts
-            v-if="renderComponent"
+            v-if="prefPopulation.length > 0 && renderComponent"
             type="chart"
             :options="chartOptions"
         />
+        <div v-else>
+            <p>都道府県が選択されていません。</p>
+        </div>
     </section>
 </template>
