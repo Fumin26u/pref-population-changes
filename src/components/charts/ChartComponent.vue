cfc2864f3f0ba0ee8d662180a7a8b9aec4620c6d
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-12-09 18:39:43 +0900

    監視対象をpropsのみにして多重にソートが実行されるのを修正
    この実装だとチェックボックス選択される度に選択された都道府県全部でAPI叩くことになってめちゃくちゃ処理時間かかりそうなので一度ブランチ切って修正する

--- src/components/charts/ChartComponent.vue
+++ src/components/charts/ChartComponent.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
 import '@/assets/css/charts/chart.css'
 import { Prefecture } from '@/assets/ts/interfaces/interfaces'
-import { ref, watchEffect } from 'vue'
+import { ref, toRefs, watch } from 'vue'
 
 interface Props {
     selectedPrefectures: Prefecture[]
 }
 
 const props = defineProps<Props>()
+const { selectedPrefectures } = toRefs(props)
 
 const prefectures = ref<Prefecture[]>([])
 // 親コンポーネントから渡った都道府県一覧を挿入し、コード順にソート
-watchEffect(() => {
-    const unSortPrefs = props.selectedPrefectures
-    prefectures.value = unSortPrefs.sort((prevPref, nextPref) => {
+watch(selectedPrefectures, () => {
+    prefectures.value = selectedPrefectures.value.sort((prevPref, nextPref) => {
         return prevPref.prefCode > nextPref.prefCode ? 1 : -1
     })
 })
