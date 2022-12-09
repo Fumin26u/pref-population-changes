99ba631e4def20cc2a4a31a8f68dd7a8873a94e1
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-12-10 05:17:47 +0900

    小一時間試行錯誤してHighchartsの実装が難しかったのでchart.jsに移行する
    単に自分のやり方が悪いだけの可能性もあるが、
    VueHighchartsコンポーネントにグラフのオプションデータが動的に渡らないのでモジュール自体の問題と踏んだ
    (自分で作ったComponentにはこのCommitの内容で正常に渡った)
    
    Chart.jsでも無理なら生highchart / chart.jsを使ってみる。

--- src/components/organisms/ChartComponent.vue
+++ src/components/organisms/ChartComponent.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import '@/assets/css/charts/chart.css'
 import VueHighcharts from 'vue3-highcharts'
-import chartOptions from '@/assets/ts/chartOptions'
+// import chartOptions from '@/assets/ts/chartOptions'
+import TestComponent from './TestComponent.vue'
 import { PrefInfo, PrefCharts } from '@/assets/ts/interfaces/interfaces'
-import { toRefs, watchEffect } from 'vue'
+import { ref, toRefs, watchEffect, computed } from 'vue'
 
 interface Props {
     prefPopulation: PrefInfo[]
@@ -12,6 +13,54 @@ interface Props {
 const props = defineProps<Props>()
 // 都道府県人口情報
 const { prefPopulation } = toRefs(props)
+const prefCharts = ref<PrefCharts[]>([])
+
+const chartOptions = computed(() => ({
+    title: {
+        text: '都道府県の人口動態',
+    },
+    xAxis: {
+        categories: [...Array(18)].map((_, index) => 1960 + index * 5),
+    },
+    yAxis: {
+        title: {
+            text: '人口(人)',
+        },
+        plotLines: [
+            {
+                value: 0,
+                width: 1,
+                color: '#888',
+            },
+        ],
+    },
+    tooltip: {
+        valueSuffix: '人',
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle',
+        borderWidth: 0,
+    },
+    series: prefCharts.value,
+    responsive: {
+        rules: [
+            {
+                condition: {
+                    maxWidth: 600,
+                },
+                chartOptions: {
+                    legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom',
+                    },
+                },
+            },
+        ],
+    },
+}))
 
 // グラフ描画用の人口情報を生成
 const generatePrefCharts = (prefs: PrefInfo[]): PrefCharts[] => {
@@ -32,12 +81,16 @@ const generatePrefCharts = (prefs: PrefInfo[]): PrefCharts[] => {
 
 // 都道府県人口情報が更新された際チャートを更新
 watchEffect(() => {
-    chartOptions.series = generatePrefCharts(prefPopulation.value)
+    prefCharts.value.splice(0)
+    generatePrefCharts(prefPopulation.value).map((pref) => {
+        prefCharts.value.push(pref)
+    })
+    console.log(chartOptions.value.series)
 })
 </script>
 <template>
     <section class="chart-area">
-        {{ chartOptions.series }}
         <VueHighcharts type="chart" :options="chartOptions" />
+        <TestComponent :options="chartOptions" />
     </section>
 </template>
