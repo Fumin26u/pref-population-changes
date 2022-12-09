807de820e494e3e247e3898bb703ce2b074e5788
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-12-09 19:56:27 +0900

    選択した都道府県の人口を取得できるようになった
    とりあえず取得できるようになっただけでコードかなり散らかってるのでここから修正していく

--- src/components/charts/ChartComponent.vue
+++ src/components/charts/ChartComponent.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import '@/assets/css/charts/chart.css'
+import endpoint from '@/assets/ts/endpoint'
 import { Prefecture } from '@/assets/ts/interfaces/interfaces'
+import axios from '@/assets/plugins/setApiKey'
 import { ref, toRefs, watch } from 'vue'
 
 interface Props {
@@ -11,16 +13,47 @@ interface Props {
 const props = defineProps<Props>()
 // Propsで送られた選択された都道府県一覧
 const { selectedPrefectures } = toRefs(props)
-// 都道府県の更新さ文
+// 都道府県の更新差分
 const { prefDiffs } = toRefs(props)
 
 // 都道府県の人口情報
 const prefPopulation = ref<any>([])
 
-// 親コンポーネントから渡った都道府県一覧を挿入し、コード順にソート
-watch(selectedPrefectures, () => {
-    console.log(selectedPrefectures)
-    console.log(prefDiffs)
+// 指定した都道府県の人口情報をAPIから取得して挿入する
+const getPopulationInfo = async (
+    url: string,
+    prefCode: number
+): Promise<any> => {
+    console.log(url, prefCode)
+    return await axios
+        .get(url, {
+            params: {
+                prefCode: prefCode,
+                cityCode: '-',
+            },
+        })
+        .then((response) => {
+            return response.data.result
+        })
+        .catch((error) => {
+            console.log(error)
+            return
+        })
+}
+
+const getPrefPopulation = async (prefs: Prefecture[]): Promise<any> => {
+    const url = endpoint + 'api/v1/population/composition/perYear'
+    const populations: any = []
+
+    prefs.map(async (pref) => {
+        populations.push(await getPopulationInfo(url, pref.prefCode))
+    })
+
+    return await populations
+}
+watch(selectedPrefectures, async () => {
+    prefPopulation.value.push(await getPrefPopulation(prefDiffs.value))
+    // console.log(prefPopulation.value)
 })
 </script>
 <template>
