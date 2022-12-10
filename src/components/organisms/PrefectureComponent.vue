5312ab93f9a0aa1a28c24019f9ae8eab5d5752f0
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-12-10 23:42:38 +0900

    都道府県の選択解除時に異なる都道府県が配列から消去されていた問題を修正

--- src/components/organisms/PrefectureComponent.vue
+++ src/components/organisms/PrefectureComponent.vue
@@ -48,6 +48,7 @@ const getPushPrefInfoAt = (haystack: Pref[], needle: Pref): number => {
 // 都道府県の選択状態に変更があった場合、選択内容と人口動態内容を変更
 const onSelectPrefecture = async (pref: Pref) => {
     const prefIndex = selectedPrefectures.value.indexOf(pref)
+    console.log(prefIndex)
     if (prefIndex !== -1) {
         // checkboxで選択が解除された場合削除
         selectedPrefectures.value.splice(prefIndex, 1)
@@ -60,7 +61,7 @@ const onSelectPrefecture = async (pref: Pref) => {
             pref
         )
         prefPopulation.value.splice(
-            getPushPrefInfoAt(selectedPrefectures.value, pref),
+            getPushPrefInfoAt(prefPopulation.value, pref),
             0,
             {
                 ...pref,
@@ -70,6 +71,7 @@ const onSelectPrefecture = async (pref: Pref) => {
     }
 
     emit('setPrefPopulation', prefPopulation.value)
+    console.log(selectedPrefectures.value)
 }
 
 // viewでlabelとinputの接続のためのidを作成する
